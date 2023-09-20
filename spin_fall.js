7225f485c92725c33124559a718d3f788c139366
Aaron Raycove <aacra0820@gmail.com>
2023-09-19 19:55:27 -0600

    attempt 0012 million

--- spin_fall.js
+++ spin_fall.js
@@ -20,7 +20,7 @@ window.addEventListener('scroll', () => {
     let delay = 0.0075;
     document.querySelectorAll('.square').forEach(square => {
         square.style.transformOrigin = 'center';
-        square.style.transform += `translateY(200vh) rotate(${Math.random() * 360}deg)`;
+        square.style.transform += `translateY(200vh) rotate(${Math.random() * 360 * 10}deg)`;
         square.style.transition = `transform 2s ease-out ${delay}s`;
         square.addEventListener('transitionend', () => {
             square.remove();

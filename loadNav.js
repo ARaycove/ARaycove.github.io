46cefafb3bd19b6b87d18bab2765b48fa08e651a
Aaron Raycove <aacra0820@gmail.com>
2023-09-20 08:14:13 -0600

    ported old site

--- loadNav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('nav.html')
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector('#navbar').innerHTML = html;
-        })
-        .catch(err => console.warn('Something went wrong.', err));
-});

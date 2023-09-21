634e8b59a03a120075f4dca5b47028fa0cca5cd2
Aaron Raycove <aacra0820@gmail.com>
2023-09-20 19:05:15 -0600

    Second js fix, added js loaded header to all pages on site

--- javascript/load_nav.js
+++ javascript/load_nav.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     fetch('/template_theme/nav.html')
         .then(response => response.text())
         .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
+            document.getElementById('nav_placeholder').innerHTML = data;
         })
         .catch(error => {
             console.error('Error:', error);

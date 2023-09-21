9d7ebd19a8aa6265c5ce96a12286876285182171
Aaron Raycove <aacra0820@gmail.com>
2023-09-20 18:44:38 -0600

    set min-height on footer and adjusting github link

--- javascript/load_nav.js
+++ javascript/load_nav.js
@@ -1,7 +1,7 @@
 fetch('/template_theme/nav.html')
     .then(response => response.text())
     .then(data => {
-        document.querySelector('#footer').innerHTML = data;
+        document.body.insertAdjacentHTML('beforeend', data);
     })
     .catch(error => {
         console.error('Error:', error);
